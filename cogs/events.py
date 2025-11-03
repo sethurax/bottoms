881b34a2015fee1b40e9b6c046f5fe17a9bdaccd
Seth Morgan <seth@sethmorgan.io>
2025-11-03 12:06:50 -0800

    adjust claim reset task to midnight UST, add handler to wait for bot readiness

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,5 @@
 import time
-# import datetime
+import datetime
 
 import discord as d
 from discord.ext import commands as c, tasks
@@ -23,10 +23,14 @@ class EventsCog(c.Cog):
             f"Time: {time.strftime('%H:%M:%S', time.localtime())} | Command: /{ctx.command.qualified_name} | Guild: {ctx.guild.name if ctx.guild else 'DM'} | User: {ctx.author.display_name}"
         )
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(time = datetime.time(hour=0, minute=0, second=0, microsecond=0))
     async def reset_claims(self):
         print(f"Claims have been reset. Next reset: {self.reset_claims.next_iteration.timestamp()}")
 
+    @reset_claims.before_loop
+    async def before_reset_claims(self):
+        await self.bot.wait_until_ready()
+        print(f"Starting claim reset scheduled task. Next reset: {self.reset_claims.next_iteration.timestamp()}")
 
 
 def setup(bot: d.Bot):
